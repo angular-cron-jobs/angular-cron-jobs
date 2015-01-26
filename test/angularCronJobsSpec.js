58c95df8a11463c98a8fc48e3a3de1bde600d35e
jacobscarter <jacob@ieksolutions.com>
2015-01-26 15:18:42 -0700

    added tests for all options

--- test/angularCronJobsSpec.js
+++ test/angularCronJobsSpec.js
@@ -1,210 +1,112 @@
-describe( 'AngularWizard', function() {
-    var $compile, $rootScope, WizardHandler;
+describe('AngularCronJobs', function() {
+    var $compile, $rootScope, cronService;
 
     beforeEach(module('angular-cron-jobs'));
 
 
-    beforeEach(inject(function(_$compile_, _$rootScope_, _WizardHandler_) {
+    beforeEach(inject(function(_$compile_, _$rootScope_, _cronService_) {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
-        WizardHandler = _WizardHandler_;
+        cronService = _cronService_;
     }));
 
-    /**
-     * Create the view with wizard to test
-     * @param  {Scope} scope         A scope to bind to
-     * @return {[DOM element]}       A DOM element compiled
-     */
     function createView(scope) {
-        scope.referenceCurrentStep = null;
-        var element = angular.element('<wizard on-finish="finishedWizard()" current-step="referenceCurrentStep" ng-init="msg = 14" >'
-                + '    <wz-step title="Starting" canenter="enterValidation">'
-                + '        <h1>This is the first step</h1>'
-                + '        <p>Here you can use whatever you want. You can use other directives, binding, etc.</p>'
-                + '        <input type="submit" wz-next value="Continue" />'
-                + '    </wz-step>'
-                + '    <wz-step title="Continuing" canexit="stepValidation">'
-                + '        <h1>Continuing</h1>'
-                + '        <p>You have continued here!</p>'
-                + '        <input type="submit" wz-next value="Go on" />'
-                + '    </wz-step>'
-                + '    <wz-step title="More steps" canenter="enterValidation">'
-                + '        <p>Even more steps!!</p>'
-                + '        <input type="submit" wz-next value="Finish now" />'
-                + '    </wz-step>'
-                + '</wizard>');
+        var element = angular.element('<cron-selection></cron-selection>');
         var elementCompiled = $compile(element)(scope);
         $rootScope.$digest();
         return elementCompiled;
     }
 
-    it("should correctly create the wizard", function() {
+    it("cron should be set for every minute", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(WizardHandler).toBeTruthy();
-        expect(view.find('section').length).toEqual(3);
-        // expect the currect step to be desirable one
-        expect(scope.referenceCurrentStep).toEqual('Starting');
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 1;
+        scope.cron = cronService.setCron(scope.myFrequency);
+        $rootScope.$digest();
+        expect(scope.cron).toEqual('* * * * *');
     });
 
-    it( "should go to the next step", function() {
+    it("cron should be set for every hour at 10 past", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 2;
+        scope.myFrequency.pastTheHour = {};
+        scope.myFrequency.pastTheHour.value = 10;
+        scope.cron = cronService.setCron(scope.myFrequency);
         $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
+        expect(scope.cron).toEqual('10 * * * *');
     });
-    it( "should return to a previous step", function() {
+
+    it("cron should be set for every day at 4:30 AM", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 3;
+        scope.myFrequency.hourValue = {};
+        scope.myFrequency.hourValue.value = 4;
+        scope.myFrequency.minuteValue = {};
+        scope.myFrequency.minuteValue.value = 30;
+        scope.cron = cronService.setCron(scope.myFrequency);
         $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().previous();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Starting');
+        expect(scope.cron).toEqual('30 4 * * *');
     });
-    it( "should go to a step specified by name", function() {
+
+    it("cron should be set for every week on Monday at 12:45 PM", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().goTo('More steps');
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 4;
+        scope.myFrequency.weekHourValue = {};
+        scope.myFrequency.weekHourValue.value = 12;
+        scope.myFrequency.weekMinuteValue = {};
+        scope.myFrequency.weekMinuteValue.value = 45;
+        scope.myFrequency.dayValue = {};
+        scope.myFrequency.dayValue.value = 2;
+        scope.cron = cronService.setCron(scope.myFrequency);
         $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('More steps');
+        expect(scope.cron).toEqual('45 12 * * 1');
     });
-    it( "should go to a step specified by index", function() {
+
+    it("cron should be set for every month on the 3rd at 6:55 PM", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().goTo(2);
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 5;
+        scope.myFrequency.weekHourValue = {};
+        scope.myFrequency.weekHourValue.value = 18;
+        scope.myFrequency.weekMinuteValue = {};
+        scope.myFrequency.weekMinuteValue.value = 55;
+        scope.myFrequency.dayOfMonthValue = {};
+        scope.myFrequency.dayOfMonthValue.value = 3;
+        scope.cron = cronService.setCron(scope.myFrequency);
         $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('More steps');
+        expect(scope.cron).toEqual('55 18 3 * *');
     });
-    it( "should go to next step becasue callback is truthy", function() {
+
+    it("cron should be set for every year on the 5th of May at 4:10 AM", function() {
         var scope = $rootScope.$new();
         var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next(function(){
-            return true
-        });
+        scope.myFrequency = {};
+        scope.myFrequency.base = {};
+        scope.myFrequency.base.value = 6;
+        scope.myFrequency.weekHourValue = {};
+        scope.myFrequency.weekHourValue.value = 4;
+        scope.myFrequency.weekMinuteValue = {};
+        scope.myFrequency.weekMinuteValue.value = 10;
+        scope.myFrequency.dayOfMonthValue = {};
+        scope.myFrequency.dayOfMonthValue.value = 5;
+        scope.myFrequency.monthValue = {};
+        scope.myFrequency.monthValue.value = 5;
+        scope.cron = cronService.setCron(scope.myFrequency);
         $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
+        expect(scope.cron).toEqual('10 4 5 5 *');
     });
-    it( "should NOT go to next step because callback is falsey", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next(function(){
-            return false
-        });
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-    });
-    it( "should go to next step because CANEXIT is UNDEFINED", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-    });
-    it( "should go to next step because CANEXIT is TRUE", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.stepValidation = function(){
-            return true;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('More steps');
-    });
-    it( "should NOT go to next step because CANEXIT is FALSE", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.stepValidation = function(){
-            return false;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-    });
-    it( "should go to next step because CANENTER is TRUE", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.enterValidation = function(){
-            return true;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('More steps');
-    });
-    it( "should NOT go to next step because CANENTER is FALSE", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.enterValidation = function(){
-            return false;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-    });
-    it( "should NOT return to a previous step. Although CANEXIT is false and we are heading to a previous state, the can enter validation is false", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.stepValidation = function(){
-            return false;
-        };
-        scope.enterValidation = function(){
-            return false;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().previous();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-    });
-    it( "should return to a previous step even though CANEXIT is false", function() {
-        var scope = $rootScope.$new();
-        var view = createView(scope);
-        scope.stepValidation = function(){
-            return false;
-        };
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().next();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Continuing');
-        WizardHandler.wizard().previous();
-        $rootScope.$digest();
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-    });
-    it( "should finish", function() {
-        var scope = $rootScope.$new();
-        var flag = false;
-        scope.finishedWizard = function() { flag = true; };
-        var view = createView(scope);
-        expect(scope.referenceCurrentStep).toEqual('Starting');
-        WizardHandler.wizard().finish();
-        expect(flag).toBeTruthy();
-        $rootScope.$digest();
-    });
+
 });
