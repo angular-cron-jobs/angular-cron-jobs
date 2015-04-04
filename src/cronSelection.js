23b97e24474f8246fd69113b7e7c547ba7402661
Wojciech Sznapka <wojciech.sznapka@cherrygroup.com>
2015-04-05 00:21:56 +0200

    Refactored setCron and added fromCron which is reverse operation. Also refactored cronselection template to be more compact.

--- src/cronSelection.js
+++ src/cronSelection.js
@@ -1,49 +1,34 @@
 angular.module('angular-cron-jobs').directive('cronSelection', ['cronService', function(cronService) {
         
-
     return {
         restrict: 'EA',
         replace: true,
         transclude: true,
         scope: {
             config : '=',
-            output : '='
+            output : '=',
+            value  : '='
         },
         templateUrl: function(element, attributes) {
           return attributes.template || "cronselection.html";
         },
         link: function($scope, $element, $attrs) {
 
-            $scope.showCustom = false;
-
             $scope.output = $scope.cron;
 
-            $scope.frequency = [
-                { 
-                    "value": 1,
-                    "text": "Minute"
-                },
-                {
-                    "value": 2,
-                    "text": "Hour"
-                },
-                {
-                    "value": 3,
-                    "text": "Day"
-                },
-                {
-                    "value": 4,
-                    "text": "Week"
-                },
-                {
-                    "value": 5,
-                    "text": "Month"
-                },
-                {
-                    "value": 6,
-                    "text": "Year"
-                }
-            ];
+            $scope.frequency = {
+                1: 'Minute',
+                2: 'Hour',
+                3: 'Day',
+                4: 'Week',
+                5: 'Month',
+                6: 'Year'
+            };
+
+            if (angular.isDefined($scope.value)) {
+                console.log('value: ' + $scope.value);
+                $scope.myFrequency = cronService.fromCron($scope.value);
+            }
 
             if(typeof $scope.config === 'object' && !$scope.config.length){
                 var optionsKeyArray = Object.keys($scope.config.options);
@@ -67,377 +52,72 @@ angular.module('angular-cron-jobs').directive('cronSelection', ['cronService', f
                 }
             }
 
+            $scope.minuteValue = ["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"];
 
-            $scope.pastTheHour = [
-                { 
-                    "value": 0
-                },
-                { 
-                    "value": 5
-                },
-                { 
-                    "value": 10
-                },
-                { 
-                    "value": 15
-                },
-                { 
-                    "value": 20
-                },
-                { 
-                    "value": 25
-                },
-                { 
-                    "value": 30
-                },
-                { 
-                    "value": 35
-                },
-                { 
-                    "value": 40
-                },
-                { 
-                    "value": 45
-                },
-                { 
-                    "value": 50
-                },
-                { 
-                    "value": 55
-                }
-            ];
+            $scope.hourValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
 
-            $scope.minuteValue = [
-                { 
-                    "value": 0
-                },
-                { 
-                    "value": 5
-                },
-                { 
-                    "value": 10
-                },
-                { 
-                    "value": 15
-                },
-                { 
-                    "value": 20
-                },
-                { 
-                    "value": 25
-                },
-                { 
-                    "value": 30
-                },
-                { 
-                    "value": 35
-                },
-                { 
-                    "value": 40
-                },
-                { 
-                    "value": 45
-                },
-                { 
-                    "value": 50
-                },
-                { 
-                    "value": 55
-                }
-            ];
+            $scope.dayValue = {
+                "0": "Sunday",
+                "1": "Monday",
+                "2": "Tuesday",
+                "3": "Wednesday",
+                "4": "Thursday",
+                "5": "Friday",
+                "6": "Saturday",
+            };
 
-            $scope.hourValue = [
-                { 
-                    "value": 0
-                },
-                { 
-                    "value": 1
-                },
-                { 
-                    "value": 2
-                },
-                { 
-                    "value": 3
-                },
-                { 
-                    "value": 4
-                },
-                { 
-                    "value": 5
-                },
-                { 
-                    "value": 6
-                },
-                { 
-                    "value": 7
-                },
-                { 
-                    "value": 8
-                },
-                { 
-                    "value": 9
-                },
-                { 
-                    "value": 10
-                },
-                { 
-                    "value": 11
-                },
-                { 
-                    "value": 12
-                },
-                { 
-                    "value": 13
-                },
-                { 
-                    "value": 14
-                },
-                { 
-                    "value": 15
-                },
-                { 
-                    "value": 16
-                },
-                { 
-                    "value": 17
-                },
-                { 
-                    "value": 18
-                },
-                { 
-                    "value": 19
-                },
-                { 
-                    "value": 20
-                },
-                { 
-                    "value": 21
-                },
-                { 
-                    "value": 22
-                },
-                { 
-                    "value": 23
-                }
-            ];
+            $scope.dayOfMonthValue = {
+                "1": "1st",
+                "2": "2nd",
+                "3": "3rd",
+                "4": "4th",
+                "5": "5th",
+                "6": "6th",
+                "7": "7th",
+                "8": "8th",
+                "9": "9th",
+                "10": "10th",
+                "11": "11th",
+                "12": "12th",
+                "13": "13th",
+                "14": "14th",
+                "15": "15th",
+                "16": "16th",
+                "17": "17th",
+                "18": "18th",
+                "19": "19th",
+                "20": "20th",
+                "21": "21st",
+                "22": "22nd",
+                "23": "23rd",
+                "24": "24th",
+                "25": "25th",
+                "26": "26th",
+                "27": "27th",
+                "28": "28th",
+                "29": "29th",
+                "30": "30th",
+                "31": "31st"
+            };
 
-            $scope.dayValue = [
-                { 
-                    "value": 1,
-                    "text": "Sunday"
-                },
-                { 
-                    "value": 2,
-                    "text": "Monday"
-                },
-                { 
-                    "value": 3,
-                    "text": "Tuesday"
-                },
-                { 
-                    "value": 4,
-                    "text": "Wednesday"
-                },
-                { 
-                    "value": 5,
-                    "text": "Thursday"
-                },
-                { 
-                    "value": 6,
-                    "text": "Friday"
-                },
-                { 
-                    "value": 7,
-                    "text": "Saturday"
-                }
-            ];
-
-            $scope.dayOfMonthValue = [
-                { 
-                    "value": 1,
-                    "text": "1st"
-                },
-                { 
-                    "value": 2,
-                    "text": "2nd"
-                },
-                { 
-                    "value": 3,
-                    "text": "3rd"
-                },
-                { 
-                    "value": 4,
-                    "text": "4th"
-                },
-                { 
-                    "value": 5,
-                    "text": "5th"
-                },
-                { 
-                    "value": 6,
-                    "text": "6th"
-                },
-                { 
-                    "value": 7,
-                    "text": "7th"
-                },
-                { 
-                    "value": 8,
-                    "text": "8th"
-                },
-                { 
-                    "value": 9,
-                    "text": "9th"
-                },
-                { 
-                    "value": 10,
-                    "text": "10th"
-                },
-                { 
-                    "value": 11,
-                    "text": "11th"
-                },
-                { 
-                    "value": 12,
-                    "text": "12th"
-                },
-                { 
-                    "value": 13,
-                    "text": "13th"
-                },
-                { 
-                    "value": 14,
-                    "text": "14th"
-                },
-                { 
-                    "value": 15,
-                    "text": "15th"
-                },
-                { 
-                    "value": 16,
-                    "text": "16th"
-                },
-                { 
-                    "value": 17,
-                    "text": "17th"
-                },
-                { 
-                    "value": 18,
-                    "text": "18th"
-                },
-                { 
-                    "value": 19,
-                    "text": "19th"
-                },
-                { 
-                    "value": 20,
-                    "text": "20th"
-                },
-                { 
-                    "value": 21,
-                    "text": "21st"
-                },
-                { 
-                    "value": 22,
-                    "text": "22nd"
-                },
-                { 
-                    "value": 23,
-                    "text": "23rd"
-                },
-                { 
-                    "value": 24,
-                    "text": "24th"
-                },
-                { 
-                    "value": 25,
-                    "text": "25th"
-                },
-                { 
-                    "value": 26,
-                    "text": "26th"
-                },
-                { 
-                    "value": 27,
-                    "text": "27th"
-                },
-                { 
-                    "value": 28,
-                    "text": "28th"
-                },
-                { 
-                    "value": 29,
-                    "text": "29th"
-                },
-                { 
-                    "value": 30,
-                    "text": "30th"
-                },
-                { 
-                    "value": 31,
-                    "text": "31st"
-                }
-            ];
-
-            $scope.monthValue = [
-                { 
-                    "value": 1,
-                    "text": "January"
-                },
-                { 
-                    "value": 2,
-                    "text": "February"
-                },
-                { 
-                    "value": 3,
-                    "text": "March"
-                },
-                { 
-                    "value": 4,
-                    "text": "April"
-                },
-                { 
-                    "value": 5,
-                    "text": "May"
-                },
-                { 
-                    "value": 6,
-                    "text": "June"
-                },
-                { 
-                    "value": 7,
-                    "text": "July"
-                },
-                { 
-                    "value": 8,
-                    "text": "August"
-                },
-                { 
-                    "value": 9,
-                    "text": "September"
-                },
-                { 
-                    "value": 10,
-                    "text": "October"
-                },
-                { 
-                    "value": 11,
-                    "text": "November"
-                },
-                { 
-                    "value": 12,
-                    "text": "December"
-                }
-            ];
-
+            $scope.monthValue = {
+                "1": "January",
+                "2": "February",
+                "3": "March",
+                "4": "April",
+                "5": "May",
+                "6": "June",
+                "7": "July",
+                "8": "August",
+                "9": "September",
+                "10": "October",
+                "11": "November",
+                "12": "December"
+            };
 
            $scope.$watch('myFrequency', function(n){
                 $scope.output = cronService.setCron(n);
-                console.log('output: ', $scope.output);
             }, true);
-
-           
-
         }
     };
 }]);
