822e106945a3fc0b18e6c2f860e7426c276f0b18
jacobscarter <jacob@ieksolutions.com>
2016-09-13 23:40:34 -0700

    update version numbers

--- Gruntfile.js
+++ Gruntfile.js
@@ -126,21 +126,14 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-bower-task');
-
   grunt.renameTask("bower", "bowerInstall");
-
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-karma');
-
   grunt.loadNpmTasks('grunt-conventional-changelog');
-
   grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-html2js');
-
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task.
